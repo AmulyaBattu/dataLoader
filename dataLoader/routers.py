65812da476d356f8a4f0f108281cbb9cd25081da
Amulya Battu <battu.amulya@gmail.com>
2018-05-07 14:01:27 -0400

    Using Different database named "primary"

--- dataLoader/routers.py
+++ dataLoader/routers.py
@@ -3,14 +3,20 @@ import random
 class PrimaryReplicaRouter:
 
     def db_for_read(self, model, **hints):
-        return random.choice(['replica1', 'replica2'])
+    # read from another db (different from insertion db)
+    # return random.choice(['replica1', 'replica2'])
+        return 'primary'
 
     def db_for_write(self, model, **hints):
         return 'primary'
 
     def allow_relation(self, obj1, obj2, **hints):
-        db_list = ('primary', 'replica1', 'replica2')
-        if obj1._state_db in db_list or obj2._state_db in db_list:
+    #
+    #    db_list = ('primary', 'replica1', 'replica2')
+    #    if obj1._state_db in db_list or obj2._state_db in db_list:
+    #        return True
+        db = 'primary'
+        if obj1._state_db == db or obj2._state_db == db:
             return True
         return None
 
